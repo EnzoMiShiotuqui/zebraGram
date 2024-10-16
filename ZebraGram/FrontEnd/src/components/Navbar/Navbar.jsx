1c8eb7370da88e778eb212d87d00bf2e6d129a27
EnzoMiShiotuqui <eshiotuqui@outlook.com>
2024-10-16 12:12:20 -0300

    feat: navbar dinamica

--- ZebraGram/FrontEnd/src/components/Navbar/Navbar.jsx
+++ ZebraGram/FrontEnd/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,18 @@ import {
   BsFillCameraFill,
 } from "react-icons/bs";
 
+
+// Hooks
+import { useState } from "react";
+import {useAuth} from '../../hooks/useAuth'
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+
+  const {auth} = useAuth()
+  const {user} = useSelector((state) => state.auth)
+
   return (
     <nav id="nav">
       <Link to="/">Zebragram</Link>
@@ -18,21 +29,43 @@ const Navbar = () => {
         <input type="text" placeholder="Pesquisar"/>
       </form>
       <ul id="nav-links">
-        <li>
-          <NavLink to="/">
-            <BsHouseDoorFill />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">
-            Entrar
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">
-            Registrar
-          </NavLink>
-        </li>
+      {auth ? (
+        <>
+          <li>
+            <NavLink to="/">
+              <BsHouseDoorFill />
+            </NavLink>
+          </li>
+          {user && (
+            <li>
+              <NavLink to={`/users/${user._id}`}>
+                <BsFillCameraFill/>
+              </NavLink>
+            </li>
+          )}
+          <li>
+            <NavLink to='/profile'>
+              <BsFillPersonFill/>
+            </NavLink>
+          </li>
+          <li>
+            <span>Sair</span>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <NavLink to="/login">
+              Entrar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">
+              Registrar
+            </NavLink>
+          </li>
+        </>
+      )}
       </ul>
     </nav>
   )
